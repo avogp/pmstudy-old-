47aa7eb8e1d4868436cf0a2b03de56393499adc1
A <>
2023-08-26 18:00:32 +0800

    new file created

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const posts = []
+
+const server = http.createServer((req,res){
+she
+        if (req.url === '/'){
+            res.write("Posts:")
+            res.write(JSON.stringify(posts))
+        }
+
+
+
+
+})
